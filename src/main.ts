2fcc5ec9d7610bd0803d370c6f0503ff30245312
ibsiany <ibsianyd@gmail.com>
2023-10-19 20:30:36 -0300

    feat: start project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
+import * as swaggerStats from 'swagger-stats';
 import { AppModule } from './app.module';
+import { initial } from './system/utils/initial';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
+const getHost = (): string => {
+  const port = process.env.PORT || 3333;
+
+  return process.env.HOST || `http://localhost:${port}`;
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Supplier Portal')
+    .setDescription('Supplier Portal endpoints')
+    .setVersion('1.0')
+    .addServer(getHost())
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(
+    swaggerStats.getMiddleware({
+      swaggerSpec: document,
+      swaggerOnly: true,
+      name: 'supplier-portal-service',
+      hostname: getHost(),
+      uriPath: '/observability',
+    }),
+  );
+
   await app.listen(process.env.PORT || 3333);
+
+  initial(getHost());
 }
+
 bootstrap();
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -12,7 +12,7 @@ export default new DataSource({
   port: configService.get('POSTGRESQL_PORT') || 5432,
   username: 'postgres',
   password: configService.get('POSTGRESQL_PASSWORD'),
-  database: configService.get('DB_NAME'),
+  database: process.env.NODE_ENV === 'test' ? 'test' : 'chameleon',
   entities: ['src/**/**/entities/*.entity{.ts,.js}'],
   logging: false,
   synchronize: false,
