22af5d99218ce9bc051d16c47109c39bc2b85d23
ibsiany <ibsianyd@gmail.com>
2023-10-31 17:59:49 -0300

    chore: update documentation

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -4,10 +4,12 @@ import { UserEntity } from './entities/user.entity';
 import { UserRepository } from './repositories/user.repository';
 import { CreateUserController } from './useCases/createUser/create-user.controller';
 import { CreateUserUseCase } from './useCases/createUser/create-user.usecase';
+import { DeleteUserController } from './useCases/deleteUser/delete-user.controller';
+import { DeleteUserUseCase } from './useCases/deleteUser/delete-user.usecase';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [CreateUserUseCase, UserRepository],
-  controllers: [CreateUserController],
+  providers: [CreateUserUseCase, UserRepository, DeleteUserUseCase],
+  controllers: [CreateUserController, DeleteUserController],
 })
 export class UserModule {}
